13c89767063f10c7df7ff24198e6cb6bc73974da
Nick Playfair <nick@chaosemerald.co.uk>
2018-07-23 13:39:40 +0100

    find the correct days reading from the array

---
+++ dist/js/script.js
@@ -0,0 +1,44 @@
+document.getElementById('pollen').addEventListener('click', getCount);
+
+//Get today's date
+var today = new Date().toISOString().substr(0,10);
+
+//Polyfill for non-ES6
+if ( !String.prototype.contains ) {
+    String.prototype.contains = function() {
+        return String.prototype.indexOf.apply( this, arguments ) !== -1;
+    };
+}
+
+
+
+
+function getCount(e) {
+    //Stop form submitting
+    e.preventDefault();
+
+    //Get lat/long
+    let loc = 'https://cors-anywhere.herokuapp.com/' +
+    'https://socialpollencount.co.uk/api/forecast?location=[' +
+    document.getElementById('location').value + ']';
+
+    //Fetch from API
+    fetch(loc, {
+      method: "GET",
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let currentLevel = 'Unknown';
+      data.forecast.forEach(function(reading) {
+        if(reading.date.contains(today)) {
+          //This is todays reading
+          console.log("todays reading found");
+          console.log(reading.pollen_count);
+          document.getElementById('levelText').innerHTML = reading.pollen_count;
+        }
+      })
+
+    })
+    .catch((err) => console.log(err))
+}
